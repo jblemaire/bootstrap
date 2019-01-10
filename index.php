fdf85943bbe9b28cfa8eaa21f2df3182f2fa6628
jblemaire <43741490+jblemaire@users.noreply.github.com>
2019-01-10 20:44:43 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>CAPEB</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  
+  <!-- Main navigation -->
+  <header>
+    <!-- Full Page Intro -->
+    <div class="view" style="background-image: url('img/capeb/background.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+      <!-- Mask & flexbox options-->
+      <div class="mask rgba-gradient align-items-center">
+        <!-- Content -->
+        <div class="container">
+          <!--Grid row-->
+          <div class="row">
+            <!--Grid column-->
+            <div class="col-md-6 white-text text-center text-md-left mt-xl-5 mb-5 wow " data-wow-delay="0.3s">
+              <img src="img/capeb/logo_capeb.png" alt="" class="img-fluid">
+              <br>
+              <h6 class="mb-3">
+                Pour votre iPhone ou Android, la CAPEB est 
+                plus proche de vous à présent : 
+                actualités, formations, boites à outils, notifications, etc.
+              </h6>
+              <div class="row">
+                <div class="col-sm-6">
+                  <input type="image" href="#" src="img/capeb/badge_android.png" alt="bouton GooglePlay" class="img-fluid">
+                </div>
+                
+                <div class="col-sm-6">
+                  <input type="image" href="#" src="img/capeb/badge_apple.png" alt="bouton appStore" class="img-fluid">
+                </div>
+              </div>
+            </div>
+            <!--Grid column-->
+            <!--Grid column-->
+            <div class="col-md-6 col-xl-5 mt-xl-5 wow " data-wow-delay="0.3s">
+              <img src="img/capeb/capeb_app_screen.png" alt="" class="img-fluid">
+            </div>
+            <!--Grid column-->
+          </div>
+          <!--Grid row-->
+        </div>
+        <!-- Content -->
+      </div>
+      <!-- Mask & flexbox options-->
+    </div>
+    <!-- Full Page Intro -->
+  </header>
+  <!-- Main navigation -->
+  <!--Main Layout-->
+  <main>
+    <div class="container">
+      <!--Grid row-->
+      <div class="row py-5">
+        <!--Grid column-->
+        <div class="col-md-10 text-center">
+          <p class="h5">Besoin d'une présentation rapide par email ? Avec plaisir, saisissez votre email et vous recevrez immédiatement un très joli email.</p>
+        </div>
+        <!--Grid column-->
+      </div>
+      <div class="row">
+        <div class=" col-md-12 text-center input-group md-form form-sm form-2 pl-0">
+          <input class="form-control my-0 py-1 black-border" type="text" placeholder="Email" aria-label="Search">
+          <div class="input-group-append">
+            <span class="input-group-text black lighten-3" id="basic-text1"><button type="button" class="btn btn-black">Recevoir la présentation</button></span>
+          </div>
+        </div>
+      </div>
+      <!--Grid row-->
+    </div>
+  </main>
+  
+  <!-- Footer -->
+  <footer class="page-footer font-small elegant-color darken-3">
+    
+    <!-- Footer Elements -->
+    <div class="container">
+      <!-- Grid row-->
+      <div class="row">
+        <!-- Grid column -->
+        <div class="col-md-12  py-5 mx-5">
+          <div class="mb-5 flex-center">
+            <!-- Facebook -->
+              <div class="col-sm-6 col-lg-3 text-center">
+                  <a class="fb-ic">
+                      <i class="fab fa-facebook-f fa-lg white-text   fa-2x"> </i>
+                  </a>
+              </div>
+            <a class="fb-ic">
+                <i class="grey-text mr-md-5  fa-2x"> / </i>
+            </a>
+            
+            <!-- Twitter -->
+              <div class="col-sm-6 col-lg-3 text-center">
+            <a class="tw-ic">
+              <i class="fab fa-twitter fa-lg white-text   fa-2x"></i>
+            </a>
+              </div>
+            <a class="fb-ic">
+                <i class="grey-text mr-md-5  fa-2x"> / </i>
+            </a>
+
+            <!-- Mail -->
+              <div class="col-sm-6 col-lg-3 text-center">
+            <a class="gplus-ic">
+              <i class="far fa-envelope fa-lg white-text   fa-2x"></i>
+              <!-- <i class="fab fa-google-plus-g fa-lg white-text mr-md-5  fa-2x"> </i> -->
+            </a>
+              </div>
+            <a class="fb-ic">
+                <i class="grey-text   fa-2x"> / </i>
+            </a>
+
+            <!--Wifi -->
+              <div class="col-sm-6 col-lg-3 text-center">
+            <a class="li-ic">
+              <i class="fas fa-wifi fa-lg white-text   fa-2x"></i>
+              <!-- <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i> -->
+            </a>
+              </div>
+          </div>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row-->
+    </div>
+    <!-- Footer Elements -->
+    
+    <!-- Copyright -->
+    <div class="footer-copyright black text-center py-3">MIW 
+      <a href="#"> ©2019</a>
+    </div>
+    <div class="footer-copyright text-center py-3"> Besoin d'un stage ? :
+        <a href="https://www.websenso.com"> https://www.websenso.com</a>
+    </div>
+    <!-- Copyright -->
+    
+  </footer>
+  <!-- Footer -->
+
+      
+      <!-- SCRIPTS -->
+      <!-- JQuery -->
+      <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+      <!-- Bootstrap tooltips -->
+      <script type="text/javascript" src="js/popper.min.js"></script>
+      <!-- Bootstrap core JavaScript -->
+      <script type="text/javascript" src="js/bootstrap.min.js"></script>
+      <!-- MDB core JavaScript -->
+      <script type="text/javascript" src="js/mdb.min.js"></script>
+      <script>
+        // Animations init
+        new WOW().init();
+      </script>
+    </body>
+    
+    </html>
